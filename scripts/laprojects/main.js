ae8d5ea6f24709d6df0b311073fb18206d6e8cc0
benblogging <85029125+benblogging@users.noreply.github.com>
2024-03-02 23:29:40 +0700

    Update main.js

--- scripts/laprojects/main.js
+++ scripts/laprojects/main.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-async function getFirestoreDB(dataName){
+export async function getFirestoreDB(dataName){
   let firestore=getFirestore();
   const dataLocation=doc(firestore, `la-data/${dataName}`);
   const dataTop=await getDoc(dataLocation);
