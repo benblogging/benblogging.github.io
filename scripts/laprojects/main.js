e0c84fce25e6259b97a7b7a16b5595889ab82651
benblogging <85029125+benblogging@users.noreply.github.com>
2024-03-02 23:56:45 +0700

    Update main.js

--- scripts/laprojects/main.js
+++ scripts/laprojects/main.js
@@ -16,11 +16,7 @@ const analytics = getAnalytics(app);
 export async function getFirestoreDB(dataName){
   let firestore=getFirestore();
   const dataLocation=doc(firestore, `la-data/${dataName}`);
-  console.log(dataLocation);
   const dataTop=await getDoc(dataLocation);
-  console.log(dataTop);
   const dataJSON=dataTop.data();
-  console.log{`"dataJSON" = 
-  ${dataJSON}`}
   return dataJSON;
 }
